8ff10326a320029d716255d34858a2d1b8e336bd
aredsterz <aredsterz@gmail.com>
2024-09-04 02:19:59 +0800

    Please help me

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,10 +4,5 @@ async function getIP() {
     document.getElementById("ipv4").innerHTML = "IPv4: " + json.ip;
     document.getElementById("country").innerHTML = "Country: " + json.country;
     document.getElementById("region").innerHTML = "Region: " + json.region;
-    document.getElementById("city").innerHTML = "City: " + json.city;
-    document.getElementById("postalCode").innerHTML = "Postal Code: " + json.postal;
-    document.getElementById("isp").innerHTML = "ISP/ORG: " + json.org;
-    document.getElementById("loc").innerHTML = "Coordinate: " + json.loc;
-    document.getElementById("timezone").innerHTML = "Time Zone: " + json.timezone;
 }
 getIP();
